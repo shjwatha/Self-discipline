f7218172a1bad6689f34301cf656cfc1899d3ce5
shjwatha <sh@jwatha.com>
2025-05-15 19:34:33 +0300

    Update UserDashboard.py

--- pages/UserDashboard.py
+++ pages/UserDashboard.py
@@ -342,13 +342,3 @@ with tabs[2]:
         st.markdown(result_df.to_html(escape=False, index=False), unsafe_allow_html=True)
 
     
-    result_df = pd.DataFrame(totals, columns=["المجموع"])
-    result_df.index.name = "البند"
-    result_df = result_df.reset_index()
-    result_df = result_df.sort_values(by="المجموع", ascending=True)
-
-    result_df = result_df[["المجموع", "البند"]]
-    result_df["البند"] = result_df["البند"].apply(lambda x: f"<p style='color:#8B0000; text-align:center'>{x}</p>")
-    result_df["المجموع"] = result_df["المجموع"].apply(lambda x: f"<p style='color:#000080; text-align:center'>{x}</p>")
-
-    st.markdown(result_df.to_html(escape=False, index=False), unsafe_allow_html=True)
