01b2992f48b2f1057fc031592549b680d3fbb9e5
shjwatha <sh@jwatha.com>
2025-05-16 00:11:58 +0300

    Update Supervisor.py

--- pages/Supervisor.py
+++ pages/Supervisor.py
@@ -153,23 +153,16 @@ def show_chat_supervisor():
                 else:
                     st.markdown(f"<p style='color:#000080'><b> {msg['from']}:</b> {msg['message']}</p>", unsafe_allow_html=True)
 
-        new_msg = st.text_area("✏️ اكتب رسالتك هنا", height=100, key="chat_message")
+        new_msg = st.text_area("✏️ اكتب رسالتك", height=100, key="chat_message")
         if st.button("📨 إرسال الرسالة"):
-            if new_msg.strip():  # تأكد أن الرسالة ليست فارغة
+            if new_msg.strip():
                 timestamp = (datetime.utcnow() + pd.Timedelta(hours=3)).strftime("%Y-%m-%d %H:%M:%S")
                 chat_sheet.append_row([timestamp, username, selected_user, new_msg, ""])
-        
-        # تم إرسال الرسالة
+                st.session_state["chat_message"] = ""
                 st.success("✅ تم إرسال الرسالة")
-
-        # مسح النص في الحقل النصي بعد الإرسال
-                st.session_state["chat_message"] = ""  # أعد تعيين القيمة إلى فارغ بعد الإرسال
-        
+                st.rerun()
             else:
                 st.warning("⚠️ لا يمكن إرسال رسالة فارغة.")
-
-
-
 
 # ===== تبويب 1: تقرير إجمالي =====
 with tabs[0]:
